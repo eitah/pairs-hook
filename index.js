f13fe155b32b1ef9afae2c7eb1e683abe6cd0d77
Elijah Itah and Gaurav Bansode <eitah+gbansode@cars.com>
2017-12-14 10:18:53 -0600

    More natural flow in logic

--- index.js
+++ index.js
@@ -10,45 +10,61 @@ function getPair() {
     return chalk.cyan(foo.split(':')[1].trim());
 }
 
-let pair = getPair();
-if (pair.split(' ').includes('and')) {
-    pair = `pair ${pair}`;
+let originalPair, currentPair;
+currentPair = originalPair = getPair();
+if (originalPair.split(' ').includes('and')) {
+    currentPair = `pair ${currentPair}`;
 }
 
-// happy path
-inquirer.prompt([
-    {
-        type: 'confirm',
-        name: 'shouldICommit',
-        message: `Are you sure you want to commit as ${pair}?`,
+let shouldICommitPrompt = {
+    type: 'confirm',
+    name: 'shouldICommit',
+    message: `Are you sure you want to commit as ${currentPair}?`,
+};
+
+let initialsInput = {
+    type: 'input',
+    name: 'initials',
+    message: 'What initial(s) would you like to use?',
+    validate(value = '') {
+        const initials = value.split(' ').filter(i => i.length === 2); // checks initials are exactly 2 characters long
+        const pass = initials.length === 1 || initials.length === 2; // if there is 1 or 2 sets of initials
+        if (pass) {
+            console.error('\n');
+            shell.exec(`git pair ${value}`);
+            return true
+        } else {
+            console.log('\nSorry please enter valid initials.');
+        }
     },
-])
+};
+
+// happy path
+
+inquirer.prompt(shouldICommitPrompt)
     .then((answer) => {
         if (answer.shouldICommit) {
+            console.error('Great!');
             shell.exit(0); // Success
         }
-    })
-    .then(() => {
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'pair',
-                message: 'What initial(s) would you like to use?',
-                validate(value = '') {
-                    const initials = value.split(' ').filter(i => i.length === 2); // checks initials are exactly 2 characters long
-                    const pass = initials.length === 1 || initials.length === 2; // if there is 1 or 2 sets of initials
-                    if (pass) {
-                        console.error('\n');
-                        shell.exec(`git pair ${value}`);
-                        shell.echo(`\nGreat, ready to commit as ${getPair()}\n`);
-                        const directions = chalk.cyan('Please copy the below into your command line');
-                        shell.echo(directions);
-                        shell.echo('\ngit commit --amend --reset-author --no-edit -n\n'); // -n ensures git will not re-run your tests
-                        shell.exit(0); // Success
-                    } else {
-                        console.log('\nSorry please enter a valid pair.');
-                    }
-                },
-            },
-        ]);
+        inquirer.prompt(initialsInput)
+            .then(() => {
+                currentPair = getPair();
+                let modifier = (currentPair !== originalPair) ? 'set to' : 'remains';
+                shell.echo(`\nGit pair ${modifier} ${currentPair}\n`);
+                inquirer.prompt({
+                    type: 'confirm',
+                    name: 'confirmPair',
+                    message: `Proceed with pair ${currentPair}?`,
+                })
+                    .then((answer) => {
+                        if (answer.confirmPair) {
+                            shell.exec('\ngit commit --amend --reset-author --no-edit -n\n');
+                            shell.echo(`\nCommitted as ${currentPair}\n`);
+                            shell.exit(0); // Success
+                        } else {
+                            console.log('That\'s ok. Execute', chalk.cyan('npm run pairs-hook'), 'to try again.')
+                        }
+                    })
+            })
     });
